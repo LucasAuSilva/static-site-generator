3f7f4e0a5710607f51d5713909cabf484569ed2c
LucasAuSilva <silvaaugustolucas@gmail.com>
2025-05-21 16:37:59 -0300

    move convert function to textnode file

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     TEXT = "text"
     NORMAL = "normal"
@@ -28,3 +30,21 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(text_node.text)
+        case TextType.BOLD:
+            return LeafNode(text_node.text, "b")
+        case TextType.ITALIC:
+            return LeafNode(text_node.text, "i")
+        case TextType.CODE:
+            return LeafNode(text_node.text, "code")
+        case TextType.LINK:
+            props = { "href": text_node.url }
+            return LeafNode(text_node.text, "a", props)
+        case TextType.IMAGE:
+            props = { "src": text_node.url, "alt": text_node.text }
+            return LeafNode("", "img", props)
+        case _:
+            raise Exception(f"invalid text type: {text_node.text_type}")
